29b8d7dbfa8e225a9427a595241fdb582becf5c0
Rodrigo Palza Gonzalez <programaro07@gmail.com>
2022-06-08 20:30:39 -0400

    se actualiza producto.handlebars y los productos se trabajan como modulo, importandolo

---
+++ productos.js
@@ -0,0 +1,34 @@
+const productos = [
+    {
+      nombre: "Plátano",
+      img: "platano.png",
+      precio: 1500,
+    },
+    {
+      nombre: "Cebollas",
+      img: "cebollas.png",
+      precio: 950,
+    },
+    {
+      nombre: "Lechuga",
+      img: "lechuga.png",
+      precio: 990,
+    },
+    {
+      nombre: "Papas",
+      img: "papas.png",
+      precio: 780,
+    },
+    {
+      nombre: "Pimentón",
+      img: "pimenton.png",
+      precio: 700,
+    },
+    {
+      nombre: "Tomate",
+      img: "tomate.png",
+      precio: 2500,
+    }
+  ]
+
+module.exports = productos
